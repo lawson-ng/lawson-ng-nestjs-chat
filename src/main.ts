c9c307fee8998f80570205c063bb5c025a3748d4
lawson-ng <abraham.lawson.dev@gmail.com>
2024-03-15 23:32:42 +0700

    init swagger

--- src/main.ts
+++ src/main.ts
@@ -2,6 +2,7 @@ import { NestFactory } from '@nestjs/core';
 import { AppModule } from './app.module';
 import { ValidationPipe } from '@nestjs/common';
 import { TransformResponseInterceptor } from './interceptors/tranform-response.interceptor';
+import { DocumentBuilder, SwaggerModule } from '@nestjs/swagger';
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
@@ -10,6 +11,15 @@ async function bootstrap() {
   app.enableCors();
   app.useGlobalPipes(new ValidationPipe());
   app.useGlobalInterceptors(new TransformResponseInterceptor());
+
+  const config = new DocumentBuilder()
+    .setTitle('Chat - Lawson Nguyen')
+    .setDescription('Chat - Lawson Nguyen')
+    .setVersion('1.0')
+    .build();
+
+  const document = SwaggerModule.createDocument(app, config);
+  SwaggerModule.setup('api', app, document);
 
   await app.listen(3002);
   console.log(`Application is running on: ${await app.getUrl()}`);
