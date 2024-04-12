50eaad2dc6a75adb9756ff6af4e201d07d1a8364
lawson-ng <abraham.lawson.dev@gmail.com>
2024-04-12 22:34:29 +0700

    add metadata public route

--- src/guards/jwt-auth.guard.ts
+++ src/guards/jwt-auth.guard.ts
@@ -1,5 +1,37 @@
-import { Injectable } from '@nestjs/common';
+import {
+  ExecutionContext,
+  Injectable,
+  UnauthorizedException,
+} from '@nestjs/common';
+import { Reflector } from '@nestjs/core';
 import { AuthGuard } from '@nestjs/passport';
+import { IS_PUBLIC_KEY } from 'src/decorators/public.decorator';
 
 @Injectable()
-export class JwtGuard extends AuthGuard('jwt') {}
+export class JwtGuard extends AuthGuard('jwt') {
+  constructor(private reflector: Reflector) {
+    super();
+  }
+
+  handleRequest(err, user, info) {
+    console.log('🚀 ~ JwtGuard ~ handleRequest ~ info:', info);
+    console.log('🚀 ~ JwtGuard ~ handleRequest ~ user:', user);
+    console.log('🚀 ~ JwtGuard ~ handleRequest ~ err:', err);
+    // You can throw an exception based on either "info" or "err" arguments
+    if (err || !user) {
+      throw err || new UnauthorizedException();
+    }
+    return user;
+  }
+
+  canActivate(context: ExecutionContext) {
+    const isPublic = this.reflector.getAllAndOverride<boolean>(IS_PUBLIC_KEY, [
+      context.getHandler(),
+      context.getClass(),
+    ]);
+    if (isPublic) {
+      return true;
+    }
+    return super.canActivate(context);
+  }
+}
